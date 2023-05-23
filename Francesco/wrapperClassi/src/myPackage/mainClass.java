91128d3f873fd8a65cc1ba16ee4f38984d651b31
Francesco Balassone <77972200+balassone@users.noreply.github.com>
2023-05-23 11:30:32 +0200

    Saved Examples

---
+++ Francesco/wrapperClassi/src/myPackage/mainClass.java
@@ -0,0 +1,13 @@
+package myPackage;
+
+public class mainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 5;
+		Integer b = 6;
+		b=8;
+		System.out.println(b);
+	}
+
+}
